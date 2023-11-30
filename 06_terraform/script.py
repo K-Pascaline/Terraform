8418a4ecad94db757cbf0763876eb8fa6f471c20
Pascaline <73042944+K-Pascaline@users.noreply.github.com>
2023-11-30 13:29:02 +0100

    Update script.py

--- 06_terraform/script.py
+++ 06_terraform/script.py
@@ -1,17 +1,16 @@
 import subprocess
 import sys
 
-def create_github_repo(Depot_terraform):
-    try:
-        subprocess.run(["terraform", "apply", "-auto-approve", "-var", f"repo_name={Depot_terraform}"])
-    except Exception as e:
-        print(f"Erreur lors de la création du dépôt : {e}")
+Depot_terraform = sys.argv[1]
 
+# Commande Terraform pour appliquer en utilisant le nom_du_repo fourni en argument
+def terraform_github(Depot_terraform):
+    subprocess.run= ["terraform", "apply", "-auto-approve", f"-var=Depot_terraform{Depot_terraform}", "-auto-approve", "-auto-approve", "-auto-approve",]
+
+    print("créé avec succès.")
+
+def main():
+    Depot_terraform= sys.argv[1]
+    terraform_github(Depot_terraform)
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Utilisation : python script.py Depot_terraform")
-        sys.exit(1)
-
-    Depot_terraform = sys.argv[1]
-
-    create_github_repo(Depot_terraform)
+    main()
